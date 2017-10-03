fb4b43b8f38513089e7e51859908262682113485
M Sharat Chandra <sharat910@gmail.com>
2017-10-03 15:23:13 +1100

    Create item_store.py

---
+++ item_store.py
@@ -0,0 +1,16 @@
+from __future__ import with_statement
+import threading
+
+class ItemStore(object):
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.items = []
+
+    def add(self, item):
+        with self.lock:
+            self.items.append(item)
+
+    def getAll(self):
+        with self.lock:
+            items, self.items = self.items, []
+        return items
